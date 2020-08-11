b10a6df7f948a979357b5f8a0265c67fbb74bdee
lathanliou <lltk2015@mymail.pomona.edu>
2020-08-11 15:28:07 +0100

    app

---
+++ app.R
@@ -0,0 +1,19 @@
+################################################################################
+# Running app
+#
+# Author: Lathan Liou
+# Created: # Tue Aug 11 15:26:37 2020 ------------------------------
+################################################################################
+
+library(shiny)
+library(shinydashboard)
+library(shinyjs)
+library(flexdashboard)
+library(rsconnect)
+source("ui.R")
+source("server.R")
+source("src/fit_model.R")
+fit <- readRDS("model.RDS")
+
+enableBookmarking("url")
+shinyApp(ui, server)
